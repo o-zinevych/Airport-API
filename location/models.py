dcfd2ae33594c86d53d9b2b107086176791ce989
Olha <olgazinevych@gmail.com>
2024-08-28 21:16:56 +0300

    add country, city and airport models

--- location/models.py
+++ location/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name_plural = "countries"
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=255)
+    country = models.ForeignKey(
+        Country,
+        related_name="cities",
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name_plural = "cities"
+
+    def __str__(self):
+        return f"{self.name} ({self.country.name})"
+
+
+class Airport(models.Model):
+    name = models.CharField(max_length=255)
+    closest_big_city = models.ForeignKey(
+        City,
+        related_name="airports",
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name} ({self.closest_big_city.__str__()})"
