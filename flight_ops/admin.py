fcc5dc6fdb6be325df764c9078f7ee80b2877533
Olha <olgazinevych@gmail.com>
2024-08-29 21:45:53 +0300

    create and register Crew, Route and Flight models with migrations

--- flight_ops/admin.py
+++ flight_ops/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Crew, Route, Flight
+
+admin.site.register(Crew)
+admin.site.register(Route)
+admin.site.register(Flight)
