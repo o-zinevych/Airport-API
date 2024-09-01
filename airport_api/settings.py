a484fff3c948a081d93972c7c230e35eebfaa9ce
Olha <olgazinevych@gmail.com>
2024-09-01 14:20:56 +0300

    feat: add JWT authentication with urls to obtain tokens and refresh them

--- airport_api/settings.py
+++ airport_api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -106,10 +106,23 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
 # User customization
 # https://docs.djangoproject.com/en/5.1/topics/auth/customizing/#auth-custom-user
 
 AUTH_USER_MODEL = "user.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication"
+    ]
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": False
+}
 
 
 # Internationalization
--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
 
 from .views import CreateUserView
 
 urlpatterns = [
-    path("register/", CreateUserView.as_view(), name="create-user")
+    path("register/", CreateUserView.as_view(), name="create-user"),
+    path("token/", TokenObtainPairView.as_view(), name="token-obtain-pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh")
 ]
 
 app_name = "user"
