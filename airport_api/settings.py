e87b54fc822ce953fc9baec933aec8a58d6caba7
Olha <olgazinevych@gmail.com>
2024-09-03 22:17:04 +0300

    feat: set default throttling for the project for users and anon per minute

--- airport_api/settings.py
+++ airport_api/settings.py
@@ -148,6 +148,11 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": ["flight_ops.permissions.IsAdminOrReadOnly"],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle"
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "10/minute", "user": "20/minute"}
 }
 
 SIMPLE_JWT = {
