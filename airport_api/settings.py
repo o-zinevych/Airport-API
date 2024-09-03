c373b6ad90d049dffa9f900960d9a197c7501818
Olha <olgazinevych@gmail.com>
2024-09-03 21:29:27 +0300

    feat: set project default pagination to page number with page size 10

--- airport_api/settings.py
+++ airport_api/settings.py
@@ -142,9 +142,9 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication"
     ],
-    "DEFAULT_PERMISSION_CLASSES": [
-        "flight_ops.permissions.IsAdminOrReadOnly"
-    ]
+    "DEFAULT_PERMISSION_CLASSES": ["flight_ops.permissions.IsAdminOrReadOnly"],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
 }
 
 SIMPLE_JWT = {
