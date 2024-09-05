6d7987de8423ea633dc1fa39717cfbb5e0de70e0
Olha <olgazinevych@gmail.com>
2024-09-05 20:54:10 +0300

    test: add airplane type and airplane model tests

--- fleet/tests/test_fleet.py
+++ fleet/tests/test_fleet.py
@@ -1,3 +1,5 @@
+from django.test import TestCase
+
 from fleet.models import Airplane, AirplaneType
 
 
@@ -21,3 +23,23 @@ def sample_airplane(**params):
     defaults.update(params)
 
     return Airplane.objects.create(**defaults)
+
+
+class AirplaneTypeModelTest(TestCase):
+    def test_airplane_type_str(self):
+        airplane_type = sample_airplane_type()
+        self.assertEqual(str(airplane_type), airplane_type.name)
+
+
+class AirplaneModelTest(TestCase):
+    def setUp(self):
+        self.airplane_type = sample_airplane_type()
+        self.airplane = sample_airplane(airplane_type=self.airplane_type)
+
+    def test_plane_capacity(self):
+        capacity = self.airplane.rows * self.airplane.seats_in_row
+        self.assertEqual(self.airplane.plane_capacity, capacity)
+
+    def test_airplane_str(self):
+        expected_str = f"{self.airplane.name}, type: {self.airplane_type.name}"
+        self.assertEqual(str(self.airplane), expected_str)
