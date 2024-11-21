84781d793cc8f698176d37ddc267efc0998ee852
Olha <olgazinevych@gmail.com>
2024-11-21 10:47:15 +0200

    test: add public and admin tests for the city view

--- location/tests/test_views.py
+++ location/tests/test_views.py
@@ -4,15 +4,20 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from location.models import Country
-from location.serializers import CountrySerializer
+from location.models import Country, City
+from location.serializers import CountrySerializer, CityListSerializer
 from location.tests.utils import sample_country, sample_city
 from user.tests.utils import sample_user
 
 COUNTRY_URL = reverse("location:country-list")
+CITY_URL = reverse("location:city-list")
 
 
-class PublicCountryAPITest(TestCase):
+def city_detail_url(city_id):
+    return reverse("location:city-detail", args=[city_id])
+
+
+class PublicCountryAPITests(TestCase):
     def setUp(self):
         self.client = APIClient()
         self.country = sample_country()
@@ -34,7 +39,7 @@ class PublicCountryAPITest(TestCase):
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 
-class AdminCountryAPITest(TestCase):
+class AdminCountryAPITests(TestCase):
     def setUp(self):
         self.client = APIClient()
         self.admin_user = get_user_model().objects.create_superuser(
@@ -45,4 +50,52 @@ class AdminCountryAPITest(TestCase):
     def test_create_country(self):
         payload = {"name": "US"}
         response = self.client.post(COUNTRY_URL, payload)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class PublicCityAPITests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.country = sample_country()
+        self.city = sample_city(country=self.country)
+        self.payload = {"name": "Cambridge", "country": self.country}
+
+    def test_city_list(self):
+        sample_city(name="Cambridge", country=self.country)
+        cities = City.objects.all()
+        serializer = CityListSerializer(cities, many=True)
+
+        response = self.client.get(CITY_URL)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["results"], serializer.data)
+
+    def test_city_detail(self):
+        serializer = CityListSerializer(self.city)
+        response = self.client.get(city_detail_url(self.city.id))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_create_city_forbidden_for_anon_users(self):
+        response = self.client.post(CITY_URL, self.payload)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_city_forbidden_for_auth_users(self):
+        self.client.force_authenticate(user=sample_user())
+        response = self.client.post(CITY_URL, self.payload)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class AdminCityAPITests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email="admin@admin.com", password="admin4321"
+        )
+        self.client.force_authenticate(user=self.admin_user)
+
+        country = sample_country()
+        self.payload = {"name": "Cambridge", "country": country.id}
+
+    def test_create_city(self):
+        response = self.client.post(CITY_URL, self.payload, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
